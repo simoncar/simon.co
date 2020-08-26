e4807b2b11c7b4aee249f16eb5bcbd379095278d
simoncar <simoncar@users.noreply.github.com>
2020-08-26 22:51:55 +0000

    deploy: 7a21194d30e45a55570aa70e947f0e0ced843e16

---
+++ precache-manifest.bdb5a4ea06eb568cf0d55610ca0a2f16.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4747cc608cd55b957de",
+    "url": "/static/js/app.bb455112.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "accbaa83cfe59a55f2d7",
+    "url": "/static/js/2.56412796.chunk.js"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "28d635fefad33f9599506904f92ed753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "89c707c57339a57caf1b5b0adefce795",
+    "url": "/static/js/2.56412796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "1e58911e3ab435d0c440884d207f93b3",
+    "url": "/manifest.json"
+  }
+];
